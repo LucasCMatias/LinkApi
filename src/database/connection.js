190987e4b0ba856c856be0257245670e72350926
lucas-felinto <lucasfepinto@gmail.com>
2021-02-18 17:38:02 -0300

    fixing docker errors

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,32 +2,16 @@ const mongoose = require('mongoose');
 const logger = require('../api/utils/logger');
 require('dotenv').config();
 
-// mongoose
-//   .connect(process.env.DB_STRING, { 
-//     useNewUrlParser: true, 
-//     useUnifiedTopology: true 
-//   })
-//   .then((data) => {
-//     console.log('Connection successful', data.connection.readyState)
-//   })
-//   .catch((err) => console.log('Connection failed', err));
+mongoose
+  .connect(process.env.DB_STRING, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  })
+  .then((data) => {
+    logger.info('Database connection successfull', data.connection.readyState)
+  })
+  .catch((err) => logger.erro('Database connection failed', err));
 
-const connection = async () => {
-  try {
-    mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    });
-
-    logger.info('Succesful Database Connection')
-  } catch (e) {
-    logger.error('Connection failed', e);
-  }
-}
-
-const close = async () => mongoose.connection.close();
-
-// const database = mongoose.connection;
+const database = mongoose.connection;
   
-module.exports = { connection, close };
+module.exports = database;
