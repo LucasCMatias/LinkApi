89c0736953676b4f2c15f1e7a545d884d1f5c6ee
lucas-felinto <lucasfepinto@gmail.com>
2021-02-19 11:30:58 -0300

    add and configure eslint and editorconfig

--- src/server.js
+++ src/server.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(apiPort, () => logger.info(`Server running on port ${apiPort}`));
 
