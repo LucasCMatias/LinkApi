d82c4717e52253875c892eabe88c94a775a360fc
lucas-felinto <lucasfepinto@gmail.com>
2021-02-19 11:42:53 -0300

    lint code

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,15 +3,15 @@ const logger = require('../api/utils/logger');
 require('dotenv').config();
 
 mongoose
-  .connect(process.env.DB_STRING, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+  .connect(process.env.DB_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then((data) => {
-    logger.info('Database connection successfull', data.connection.readyState)
+    logger.info('Database connection successfull', data.connection.readyState);
   })
   .catch((err) => logger.erro('Database connection failed', err));
 
 const database = mongoose.connection;
-  
+
 module.exports = database;
