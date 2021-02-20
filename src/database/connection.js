8b71c44bd15de4a7307be5d0038f85121051ed78
lucas-felinto <lucasfepinto@gmail.com>
2021-02-20 00:33:06 -0300

    fixing logger path and syntax error

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const logger = require('../api/utils/logger');
+const logger = require('../utils/logger');
 require('dotenv').config();
 
 mongoose
@@ -10,7 +10,7 @@ mongoose
   .then((data) => {
     logger.info('Database connection successfull', data.connection.readyState);
   })
-  .catch((err) => logger.erro('Database connection failed', err));
+  .catch((err) => logger.error('Database connection failed', err));
 
 const database = mongoose.connection;
 
